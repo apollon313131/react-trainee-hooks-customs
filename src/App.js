978b2ac6988c04fbf78f71ed326eb2a8915737f5
apollon313131 <apollo-bel@mail.ru>
2021-06-01 14:28:26 +0300

    add useHover / useInput

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+import useInput from './hooks/useInput';
+import Hover from './components/Hover';
+import { Card } from 'react-bootstrap';
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        header className = "App-header" >
-        <
-        img src = { logo }
-        className = "App-logo"
-        alt = "logo" / >
-        <
-        p >
-        Edit2 < code > src / App.js < /code> and save to reload. <
-        /p> <
-        a className = "App-link"
-        href = "https://reactjs.org"
-        target = "_blank"
-        rel = "noopener noreferrer" >
-        Learn React <
-        /a> <
-        /header> <
-        /div>
-    );
+  const username = useInput('')
+  const password = useInput('')
+
+  return (
+    <div className='main'>
+      <div className="useInput">
+        <h1>Hook useInput </h1>
+        <input {...username} type="text" placeholder='username' />
+        <input {...password} type="text" placeholder='password' />
+        <button onClick={() => console.log(username.value, password.value)}>Click</button>
+      </div>
+      <br /><br />
+      <div className='useHover'>
+        <h1>Hook useHover</h1>
+        <Hover />
+      </div>
+    </div>
+
+  );
 }
 
 export default App;
---
+++ src/components/Hover.jsx
@@ -0,0 +1,12 @@
+import React, { useRef } from 'react'
+import useHover from '../hooks/useHover';
+
+export default function Hover() {
+    const ref = useRef();
+    const isHovering = useHover(ref);
+    console.log(isHovering);
+    return (
+        <div ref={ref} style={{ width: 300, height: 300, background: isHovering ? 'red' : 'green' }}>
+        </div>
+    )
+}
